ba83a9d3b83de1064e51262386b19f81a15b38c7
Evaldo <contato@evaldofelipe.com>
2017-07-29 14:11:37 -0300

    changes place

--- contador1007/index.php
+++ contador1007/index.php
@@ -19,7 +19,7 @@
                 body {
             padding-top: 40px;
             padding-bottom: 40px;
-            background-image: url(../contador1007/img/bg.jpg);
+            background-image: url(bg.jpg);
             background-repeat: no-repeat;
             background-position: center center;
             background-attachment: fixed;
